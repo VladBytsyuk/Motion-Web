d1d8fb97a64dc139fe0658c1840f1e872456ace4
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2017-03-26 17:50:05 +0300

    Popup validation

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,9 @@ var callbackAction = new function() {
 	var submit = document.getElementById('popup_btn');
 
 	function open() {
+		nameField.value = '';
+		phoneField.value = '';
+		adressField.value = '';
 		popup.style.visibility = 'visible';
 		popup.style.opacity = 1;
 	}
@@ -18,8 +21,34 @@ var callbackAction = new function() {
 		popup.style.opacity = 0;
 	}
 
-	submit.addEventListener('click', function() {
+	function send(name, phone, adress) {
 		close();
+	}
+
+	submit.addEventListener('click', function() {
+		nameField.style.borderColor = '#232324';
+		phoneField.style.borderColor = '#232324';
+		adressField.style.borderColor = '#232324';
+		var nameRegEp = /^[a-zA-Z]{2,30}$/;
+		var phoneRegExp = /^[0-9]{7,16}$/;
+		var adressRegExp = /\S+@\S+\.\S+/;
+		var isBreak = false;
+		if (nameField.value === '' || !nameField.value.match(nameRegEp)) {
+			nameField.style.borderColor = '#eb4f4e';
+			isBreak = true;
+		}
+		if (phoneField.value === '' || !phoneField.value.match(phoneRegExp)) {
+			phoneField.style.borderColor = '#eb4f4e';
+			isBreak = true;
+		}
+		if (adressField.value === '' || !adressField.value.match(adressRegExp)) {
+			adressField.style.borderColor = '#eb4f4e';
+			isBreak = true;
+		}
+		if (isBreak) {
+			return;
+		}
+		send(nameField.value, phoneField.value, adressField.value);
 	});
 
 	return {
