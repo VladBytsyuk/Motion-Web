cd7c4cc967bfb81911c3297129f21b498cc3940a
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2017-05-09 10:42:05 +0300

    Validation form

--- app/assets/js/angular-init.js
+++ app/assets/js/angular-init.js
@@ -13,6 +13,39 @@ app.controller('controller', ['$scope', '$window', function($scope, $window) {
 	    		$scope.width = $window.innerWidth;
 	    	});
 	    };
+	    $scope.openPopup = () => {
+	    	$scope.isPopupOpen = true;
+	    	$scope.user.name = '';
+	    	$scope.user.number = '';
+	    	$scope.user.adress = '';
+	    };
+	    $scope.closePopup = () => {
+	    	const nameRegEp = /^[a-zA-Z]{2,30}$/;
+	        const phoneRegExp = /^[0-9]{7,16}$/;
+	        const adressRegExp = /\S+@\S+\.\S+/;
+	        let isBreak = false;
+
+	        $scope.nameHighlighted = false;
+	        $scope.numberHighlighted = false;
+	        $scope.adressHighlighted = false;
+
+	        if ($scope.user.name == '' || !$scope.user.name.match(nameRegEp)) {
+	            $scope.nameHighlighted = true;
+	            isBreak = true;
+	        }
+	        if ($scope.user.number == '' || !$scope.user.number.match(phoneRegExp)) {
+	            $scope.numberHighlighted = true;
+	            isBreak = true;
+	        }
+	        if ($scope.user.adress == '' || !$scope.user.adress.match(adressRegExp)) {
+	            $scope.adressHighlighted = true;
+	            isBreak = true;
+	        }
+
+	        if (!isBreak) {
+	        	$scope.isPopupOpen = false;
+	        }
+	    };
 	}
 ]);
 
